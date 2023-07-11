37c94e0605e61025e2715a98d238d8669772bf14
rohailparacha <41423866+rohailparacha@users.noreply.github.com>
2023-07-11 22:38:46 +0500

    Update stripe.js

--- utils/stripe.js
+++ utils/stripe.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe("sk_test_51Me7IzDtKSW92gialveTiFhdD9Uyh5hL162bkYrKYHjheA085iKm7Iirb5Ug9664zsJ0sloq2PthUP0RqsyuVPnx00JRprnoAX");
+const stripe = new Stripe(process.env.STRIPE_KEY);
 
 export default stripe;
