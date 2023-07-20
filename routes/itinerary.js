2007e917f476aba6cd5211a894a08cd631d27a45
rohailparacha <41423866+rohailparacha@users.noreply.github.com>
2023-07-20 20:30:16 +0500

    Update itinerary.js

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -2,20 +2,38 @@ import express from "express";
 import passport from "passport";
 import Itinerary from "../controllers/itinerary.js";
 import upload from "../utils/multer.js";
+import multerUpload from "../utils/multerUpload.js";
 var router = express.Router();
 
 /* GET users listing. */
-router.post("/", [passport.authenticate("jwt", { session: false }), upload.any()], Itinerary.addItinierary);
-router.get("/", Itinerary.getItineraries);
-router.get("/purchased", passport.authenticate("jwt", { session: false }), Itinerary.getPurchasedItineraries);
-router.get("/list/me", passport.authenticate("jwt", { session: false }), Itinerary.getMyItineraries);
-router.get(
-  "/sendEmail/:id",
-  passport.authenticate("jwt", { session: false }),
-  Itinerary.sendEmail
+// router.post("/", [passport.authenticate("jwt", { session: false }), upload.any()], Itinerary.addItinierary);
+router.post(
+  "/",
+  [passport.authenticate("jwt", { session: false }), multerUpload.any()],
+  Itinerary.addItinierary
 );
-router.get("/view/:itineraryId", passport.authenticate("jwt", { session: false }), Itinerary.getSingleItinerary);
-router.delete("/:itinerary", passport.authenticate("jwt", { session: false }), Itinerary.deleteItinerary);
+router.get("/", Itinerary.getItineraries);
+router.get(
+  "/purchased",
+  passport.authenticate("jwt", { session: false }),
+  Itinerary.getPurchasedItineraries
+);
+router.get(
+  "/list/me",
+  passport.authenticate("jwt", { session: false }),
+  Itinerary.getMyItineraries
+);
+router.get("/sendEmail/:id", passport.authenticate("jwt", { session: false }), Itinerary.sendEmail);
+router.get(
+  "/view/:itineraryId",
+  passport.authenticate("jwt", { session: false }),
+  Itinerary.getSingleItinerary
+);
+router.delete(
+  "/:itinerary",
+  passport.authenticate("jwt", { session: false }),
+  Itinerary.deleteItinerary
+);
 router.patch("/deleteDay", passport.authenticate("jwt", { session: false }), Itinerary.deleteDay);
 router.patch(
   "/:itineraryId",
