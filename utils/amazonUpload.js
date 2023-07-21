5a39461a8c88eae9e7ac83a95a04dbc965b9a772
rohailparacha <41423866+rohailparacha@users.noreply.github.com>
2023-07-21 23:27:20 +0500

    Update amazonUpload.js

--- utils/amazonUpload.js
+++ utils/amazonUpload.js
@@ -1,8 +1,8 @@
 import { s3Uploadv2, s3Uploadv3 } from "../utils/s3Bucket.js";
-export const mediaUpload = async (file) => {
+export const mediaUpload = async (file,key) => {
   try {
-    const results = await s3Uploadv2(file);
-    console.log(results);
+    const results = await s3Uploadv2(file,key);
+  
     return results;
   } catch (err) {
     // next(err);
