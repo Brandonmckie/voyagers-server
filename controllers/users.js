49aa8f76500b727fe1bd3f901a1d323dd89b7fbb
rohailparacha <rohail.paracha@outlook.com>
2023-10-06 17:16:23 +0500

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -952,7 +952,12 @@ class UserController {
     try {
       const user = await User.findById(req.user.id).select("+email +accountId");
 
-  
+      res.send({
+        user: {
+          ...user._doc,
+          stripeConnected: true,
+        },
+      });
       const destinationAccount = await stripe.accounts.retrieve(user.accountId);
       res.send({
         user: {
